563659ed84aed3af73a090615dd97d28b61cd675
weyewe <w.yunnal@gmail.com>
2012-12-31 17:58:29 +0700

    spec for delivery

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -1,9 +1,55 @@
 class Delivery < ActiveRecord::Base
   has_many :delivery_entries
-   
   
-  has_one :sales_invoice
+  validates_presence_of :creator_id
+  validates_presence_of :customer_id 
   
+  
+  def self.create_by_employee( employee, params ) 
+    return nil if employee.nil? 
+    
+    new_object = SalesOrder.new
+    new_object.creator_id                 = employee.id
+    new_object.customer_id                = params[:customer_id]
+    new_object.delivery_address           = params[:delivery_address]
+    new_object.delivery_date              = params[:delivery_date]
+    
+    
+    if new_object.save
+      new_object.generate_code
+    end
+    
+    return new_object 
+  end
+  
+  def generate_code
+    string = "DO" + "/" + 
+              self.created_at.year.to_s + '/' + 
+              self.created_at.month.to_s + '/' + 
+              self.id.to_s
+              
+    self.code =  string 
+    self.save 
+  end
+  
+  def confirm(employee) 
+    return nil if employee.nil? 
+    return nil if self.delivery_items.count == 0 
+    return nil if self.is_confirmed == true  
+    
+    # transaction block to confirm all the sales item  + sales order confirmation 
+    ActiveRecord::Base.transaction do
+      self.confirmer_id = employee.id 
+      self.confirmed_at = DateTime.now 
+      self.is_confirmed = true 
+      self.save 
+      self.delivery_items.each do |delivery_item|
+        delivery_item.confirm 
+      end
+    end 
+  end
+  
+    
   
   
   # attr_accessible :title, :body
--- spec/models/delivery_spec.rb
+++ spec/models/delivery_spec.rb
@@ -6,22 +6,172 @@ describe Delivery do
   
     
     @customer = FactoryGirl.create(:customer,  :name => "Weyewe",
-                                            :contact_person => "Willy" )  
+                                            :contact_person => "Willy" )
+                                            
+    @joko = FactoryGirl.create(:employee,  :name => "Joko" )
+    @joni = FactoryGirl.create(:employee,  :name => "Joni" )
+                                            
+    @sales_order   = SalesOrder.create_by_employee( @admin , {
+      :customer_id    => @customer.id,          
+      :payment_term   => PAYMENT_TERM[:cash],    
+      :order_date     => Date.new(2012, 12, 15)   
+    })
+   
+    
+    @quantity_in_sales_item = 50 
+    @complete_cycle_sales_item = SalesItem.create_sales_item( @admin, @sales_order,  {
+        :material_id => MATERIAL[:steel][:value], 
+        :is_pre_production => true , 
+        :is_production     => true, 
+        :is_post_production => true, 
+        :is_delivered => true, 
+        :delivery_address => "Perumahan Citra Garden 1 Blok AC2/3G",
+        :quantity => @quantity_in_sales_item,
+        :description => "Bla bla bla bla bla", 
+        :delivery_address => "Yeaaah babyy", 
+        :requested_deadline => Date.new(2013, 3,5 ),
+        :price_per_piece => "90000", 
+        :weight_per_piece   => '15' 
+      })
     
     
-    # create sales order
-    # create sales item
-    # confirm sales order 
-    # create production history 
-    # create post production history 
-    # create delivery order 
-    # create delivery order confirmation 
-      # => sales return
-        # => decide whether it will go to post production (fix) 
-        # => or it will go to production  (remake) 
-      # => delivery loss 
+   
+
+    @sales_order.confirm(@admin)
+    @complete_cycle_sales_item.reload
+    
+    @processed_quantity = 10
+    @ok_quantity = 8 
+    @broken_quantity = 1
+    @repairable_quantity = 1 
+    
+    @production_history = ProductionHistory.create_history( @admin, @complete_cycle_sales_item, {
+      :processed_quantity    => @processed_quantity, 
+      :ok_quantity           => @ok_quantity, 
+      :repairable_quantity   => @repairable_quantity, 
+      :broken_quantity       => @broken_quantity, 
+
+      :ok_weight             =>  "#{8*15}" ,  # in kg.. .00 
+      :repairable_weight     => '13',
+      :broken_weight         =>  "#{2*10}" ,
+
+      # :person_in_charge      => nil ,# list of employee id 
+      :start_date            => Date.new( 2012, 10,10 ) ,
+      :finish_date           => Date.new( 2013, 1, 15) 
+    })
+    
+    @initial_pending_production = @complete_cycle_sales_item.pending_production
+    @initial_pending_post_production = @complete_cycle_sales_item.pending_post_production 
+    
+    @production_history.confirm(@admin)
+    @complete_cycle_sales_item.reload
+    @pending_post_production = @complete_cycle_sales_item.pending_post_production
+    
+    @total_post_production_1 = 4 
+    
+    @post_production_broken_1 = 1 
+    @post_production_ok_1 = @total_post_production_1 - @post_production_broken_1
+    @post_production_history = PostProductionHistory.create_history( @admin, @complete_cycle_sales_item, {
+      :ok_quantity           => @post_production_ok_1, 
+      :broken_quantity       => @post_production_broken_1, 
+
+      :ok_weight             =>  "#{@post_production_ok_1*15}" ,  # in kg.. .00 
+      :broken_weight         =>  "#{@post_production_broken_1*10}" ,
+
+      # :person_in_charge      => nil ,# list of employee id 
+      :start_date            => Date.new( 2012, 10,10 ) ,
+      :finish_date           => Date.new( 2013, 1, 15) 
+    })
+    
+    @complete_cycle_sales_item.reload 
+    
+    @initial_pending_post_production = @complete_cycle_sales_item.pending_post_production 
+    @initial_ready      = @complete_cycle_sales_item.ready
+    @initial_pending_production  =  @complete_cycle_sales_item.pending_production 
+    
+    @post_production_history.confirm( @admin ) 
+
+    @complete_cycle_sales_item.reload
     
   end
+  
+  it 'should not allow sales order creation if there is no employee' do 
+    delivery   = Delivery.create_by_employee( nil , {
+      :customer_id    => @customer.id,          
+      :delivery_address   => "some address",    
+      :delivery_date     => Date.new(2012, 12, 15) 
+    })
+    
+    delivery.should be_nil 
+  end
+  
+  it 'should allow creation if there is employee' do 
+    
+    delivery   = Delivery.create_by_employee( @admin , {
+      :customer_id    => @customer.id,          
+      :delivery_address   => "some address",    
+      :delivery_date     => Date.new(2012, 12, 15)   
+    })
+    
+    delivery.should be_valid
+  end
+  
+  context "post sales order creation" do
+    before(:each) do
+      @delivery   = Delivery.create_by_employee( @admin , {
+        :customer_id    => @customer.id,          
+        :delivery_address   => "some address",    
+        :delivery_date     => Date.new(2012, 12, 15)
+      })
+    end
+    
+    it 'should not be confirmable if there is no sales item' do
+      @delivery.confirm( @admin ) 
+      @delivery.is_confirmed.should be_false 
+    end 
+    
+    # context 'creating delivery with 1 delivery entry ' do
+    #   before(:each) do
+    #     @quantity_in_delivery_entry = 50  
+    #     
+    #     if @complete
+    #     @has_production_sales_item = SalesItem.create_sales_item( @admin, @sales_order,  {
+    #         :material_id => MATERIAL[:steel][:value], 
+    #         :is_pre_production => true , 
+    #         :is_production     => true, 
+    #         :is_post_production => true, 
+    #         :is_delivered => true, 
+    #         :delivery_address => "Perumahan Citra Garden 1 Blok AC2/3G",
+    #         :quantity => @quantity_in_sales_item,
+    #         :description => "Bla bla bla bla bla", 
+    #         :delivery_address => "Yeaaah babyy", 
+    #         :requested_deadline => Date.new(2013, 3,5 ),
+    #         :price_per_piece => "90000", 
+    #         :weight_per_piece   => '15'
+    #       }) 
+    #   end
+    #   
+    #   it 'should have 1 sales_item' do
+    #     @sales_order.sales_items.count.should == 1 
+    #   end
+    #   
+    #   it 'should be confirmable' do 
+    #     @sales_order.confirm( @admin ) 
+    #     @sales_order.is_confirmed.should be_true
+    #   end
+    #   
+    # end # context 'creating delivery with 1 delivery entry , including production'
+  end
+  
+  
+  # create delivery order 
+  # => add delivery entries
+  # create delivery order confirmation 
+    # => sales return
+      # => decide whether it will go to post production (fix) 
+      # => or it will go to production  (remake) 
+    # => delivery loss 
+  
  
   
   
